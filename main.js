8223d4543aae81ccf730bdbc4a13d6f1cff77e10
wepapepa <catu.leibrin@gmail.com>
2023-12-13 16:51:38 -0300

    started the if if else section

---
+++ main.js
@@ -0,0 +1,11 @@
+let edad = Number(prompt("¿Cuántos años tienes?"));
+let cumpleanos = Number(prompt("¿Qué año naciste?"));
+
+if(edad = 16 || 17){
+    alert("¡Lo siento! Sólo puedes beber en Reino Unido, y únicamente vino, cerveza, o sidra. (Y bajo la supervición de un adulto")
+    alert("En ")
+}else if(edad >= 18){
+    alert("¡Enhorabuena! Puedes beber en casi todos los países, menos Estados Unidos, Cameroon, Egipto, Guinea Ecuatoriana, Etiopía, Canadá, Paraguay, Bahrain, Indonesia, Japón, Kazakhstan, Mongolia, Oman, Qatar, Korea del Sur, Tailandia, Turkmenistan, United Arab Emirates, y Uzbekistan.")
+} else if (edad >= 21){
+    alert("¡Mírate! Puedes beber en todos los países, menos en algunos lugares de India.")
+}
